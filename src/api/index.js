88385181e487747725e5be8a536140f9464b86ab
qc90 <qc90@foxmail.com>
2019-09-02 15:37:09 +0800

    login

--- src/api/index.js
+++ src/api/index.js
@@ -20,7 +20,7 @@ export default ({ config, db }) => {
 				req.session.apikey = apikey
 
 				const rss = await robotApi.getWechatQrcode(apikey)
-				console.log(JSON.parse(rss), 'qrcode')
+				console.log(JSON.parse(rss.data), 'qrcode')
 			}
 		} catch (e) {
 			console.log(e)

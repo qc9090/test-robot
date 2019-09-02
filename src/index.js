56a1648e7cd7fd91cda86c3eba4ac50edcc1a2be
qc90 <qc90@foxmail.com>
2019-09-02 19:36:08 +0800

    add redis

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import redis from 'redis'
 import session from 'express-session';
+
 import initializeDb from './db';
 import middleware from './middleware';
 import api from './api';
@@ -18,14 +20,16 @@ app.use(morgan('dev'));
 
 app.use(cookieParser());
 
-// session 
-app.use(session({
-	secret: 'keyboard cat',
-	key: 'admin',
-  resave: false,
-	saveUninitialized: true,
-	cookie: { secure: true }
-}))
+let RedisStore = require('connect-redis')(session)
+let client = redis.createClient()
+
+app.use(
+  session({
+    store: new RedisStore({ client }),
+    secret: 'keyboard cat',
+    resave: false,
+  })
+)
 
 // 3rd party middleware
 app.use(cors({

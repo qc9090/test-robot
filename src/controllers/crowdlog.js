a928dd39d1091b8cfb12e4427aaea5e9ecfbe021
qc90 <qc90@foxmail.com>
2019-09-29 14:03:26 +0800

    update robot

--- src/controllers/crowdlog.js
+++ src/controllers/crowdlog.js
@@ -83,17 +83,12 @@ const createDid = (wxid, ownerid, apikey, myAccount, roomid, contactName) => {
           if (method.includes('ExtrinsicFailed')) isSuccessful = false
         })
         
-        // test
-        const content = `@${contactName} 恭喜您创建PRA账户成功！`
-        const rs = await robotApi.groupAt(apikey, myAccount, roomid, wxid, content)
-        console.log(rs, '创建账号成功')
-        
         if (isSuccessful) {
           let did = await api.query.did.identity(address)
           did = hexToDid(did)
           console.log(did, 'did created')
           
-          const content = `@${wxid}恭喜您创建PRA账户成功！您的账户为${did}，该账户已经与你的微信号绑定，您在微信群中获得的收益将直接转入该账户中。您的账户链接：https://prabox.net/请收藏`
+          const content = `@${contactName} 恭喜您创建PRA账户成功！您的账户为${did}，该账户已经与你的微信号绑定，您在微信群中获得的收益将直接转入该账户中。您的账户链接：https://prabox.net/请收藏`
           const rs = await robotApi.groupAt(apikey, myAccount, roomid, wxid, content)
           console.log(rs, '创建账号成功')
         }
